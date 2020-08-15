7129e6107ad2cedecd1e23cb374a47a7064fa309
unknown <ngashbaugh@gmail.com>
2020-08-15 14:52:03 -0700

    Final Commit All Done

--- Homework/starter_code/api_keys.py
+++ Homework/starter_code/api_keys.py
@@ -1,6 +1,6 @@
 # OpenWeatherMap API Key
-weather_api_key = "5c48469fcbe2b4e8120cdca8199bd68c"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyDKaraYPrQVfTlPmZAw28X4JF95xn_pins"
+g_key = ""
 
