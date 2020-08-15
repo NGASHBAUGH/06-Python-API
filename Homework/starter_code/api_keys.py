2d20dbe2113bd2fd30bc93b2526bed64e3ef0f4d
unknown <ngashbaugh@gmail.com>
2020-08-15 11:03:35 -0700

    Test push

--- Homework/starter_code/api_keys.py
+++ Homework/starter_code/api_keys.py
@@ -1,6 +1,6 @@
 # OpenWeatherMap API Key
-weather_api_key = ""
+weather_api_key = "5c48469fcbe2b4e8120cdca8199bd68c"
 
 # Google API Key
-g_key = ""
+g_key = "AIzaSyDKaraYPrQVfTlPmZAw28X4JF95xn_pins"
 
